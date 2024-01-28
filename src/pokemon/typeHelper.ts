d2a2e5eaffc0b968582b9139440e24d3969db5fe
Brandon Ting <brandon1221t@gmail.com>
2024-01-28 16:22:58 +0800

    expose typechart

--- src/pokemon/typeHelper.ts
+++ src/pokemon/typeHelper.ts
@@ -1,4 +1,4 @@
-import abilities from "../../assets/ability.json";
-import items from "../../assets/item.json";
+import abilities from "../../data/ability.json";
+import items from "../../data/item.json";
 export type Ability = keyof typeof abilities;
 export type Item = keyof typeof items;
