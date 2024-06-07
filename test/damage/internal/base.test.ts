29fb933a9d59b9ab5e8a8d341e528302a9f123cd
Brandon Ting <brandonting@kkcompany.com>
2024-06-07 22:14:41 +0800

    test through base test

--- test/damage/internal/base.test.ts
+++ test/damage/internal/base.test.ts
@@ -10,7 +10,9 @@ test("correctly calculate base power", () => {
 		base: 100,
 	});
 	const basePower = getBasePower(testPokemon, testPokemon, testMove);
-	expect(basePower).toBe(100);
+	expect(basePower).toEqual({
+		operator: 100,
+	});
 });
 
 test("correctly calculate base power for terrain related moves", () => {
@@ -27,7 +29,14 @@ test("correctly calculate base power for terrain related moves", () => {
 			weather: "Sun",
 		},
 	);
-	expect(basePowerHydroSteam).toBe(120);
+	expect(basePowerHydroSteam).toEqual({
+		operator: 120,
+		factors: {
+			field: {
+				weather: true
+			}
+		}
+	});
 
 	const testPsyblade = createMove({
 		id: 876,
@@ -41,7 +50,14 @@ test("correctly calculate base power for terrain related moves", () => {
 			terrain: "Electric",
 		},
 	);
-	expect(basePowerPsyblade).toBe(120);
+	expect(basePowerPsyblade).toEqual({
+		operator: 120,
+		factors: {
+			field: {
+				terrain: true
+			}
+		}
+	});
 
 	const testMistyExplosion = createMove({
 		id: 802,
@@ -55,8 +71,14 @@ test("correctly calculate base power for terrain related moves", () => {
 			terrain: "Misty",
 		},
 	);
-	expect(basePowerMistyExplosion).toBe(150);
-
+	expect(basePowerMistyExplosion).toEqual({
+		operator: 150,
+		factors: {
+			field: {
+				terrain: true
+			}
+		}
+	});
 	const testEarthQuake = createMove({
 		id: 89,
 		base: 100,
@@ -69,7 +91,14 @@ test("correctly calculate base power for terrain related moves", () => {
 			terrain: "Grassy",
 		},
 	);
-	expect(baseEarthQuake).toBe(50);
+	expect(baseEarthQuake).toEqual({
+		operator: 50,
+		factors: {
+			field: {
+				terrain: true
+			}
+		}
+	});
 	const testBulldoze = createMove({
 		id: 523,
 		base: 60,
@@ -77,7 +106,14 @@ test("correctly calculate base power for terrain related moves", () => {
 	const baseBulldoze = getBasePower(testPokemon, testPokemon, testBulldoze, {
 		terrain: "Grassy",
 	});
-	expect(baseBulldoze).toBe(30);
+	expect(baseBulldoze).toEqual({
+		operator: 30,
+		factors: {
+			field: {
+				terrain: true
+			}
+		}
+	});
 
 	const testTerrainPulse = createMove({
 		id: 805,
@@ -91,7 +127,14 @@ test("correctly calculate base power for terrain related moves", () => {
 			terrain: "Grassy",
 		},
 	);
-	expect(baseTerrainPulse).toBe(100);
+	expect(baseTerrainPulse).toEqual({
+		operator: 100,
+		factors: {
+			field: {
+				terrain: true
+			}
+		}
+	});
 });
 
 test("correctly calculate base power for speed related moves", () => {
@@ -113,7 +156,7 @@ test("correctly calculate base power for speed related moves", () => {
 		testSlowPokemon,
 		testElectricBall,
 	);
-	expect(basePowerElectricBall).toBe(150);
+	expect(basePowerElectricBall.operator).toBe(150);
 
 	if (testFastPokemon.stats) testFastPokemon.stats.speed = 175;
 	basePowerElectricBall = getBasePower(
@@ -121,7 +164,7 @@ test("correctly calculate base power for speed related moves", () => {
 		testSlowPokemon,
 		testElectricBall,
 	);
-	expect(basePowerElectricBall).toBe(120);
+	expect(basePowerElectricBall.operator).toBe(120);
 
 	if (testFastPokemon.stats) testFastPokemon.stats.speed = 125;
 	basePowerElectricBall = getBasePower(
@@ -129,7 +172,7 @@ test("correctly calculate base power for speed related moves", () => {
 		testSlowPokemon,
 		testElectricBall,
 	);
-	expect(basePowerElectricBall).toBe(80);
+	expect(basePowerElectricBall.operator).toBe(80);
 
 	if (testFastPokemon.stats) testFastPokemon.stats.speed = 75;
 	basePowerElectricBall = getBasePower(
@@ -137,7 +180,7 @@ test("correctly calculate base power for speed related moves", () => {
 		testSlowPokemon,
 		testElectricBall,
 	);
-	expect(basePowerElectricBall).toBe(60);
+	expect(basePowerElectricBall.operator).toBe(60);
 
 	if (testFastPokemon.stats) testFastPokemon.stats.speed = 25;
 	basePowerElectricBall = getBasePower(
@@ -145,7 +188,7 @@ test("correctly calculate base power for speed related moves", () => {
 		testSlowPokemon,
 		testElectricBall,
 	);
-	expect(basePowerElectricBall).toBe(40);
+	expect(basePowerElectricBall.operator).toBe(40);
 
 	if (testFastPokemon.stats) testFastPokemon.stats.speed = 300;
 	const testGyroBall = createMove({
@@ -156,7 +199,7 @@ test("correctly calculate base power for speed related moves", () => {
 		testFastPokemon,
 		testGyroBall,
 	);
-	expect(basePowerGyroBall).toBe(150);
+	expect(basePowerGyroBall.operator).toBe(150);
 
 	if (testFastPokemon.stats) testFastPokemon.stats.speed = 200;
 	basePowerGyroBall = getBasePower(
@@ -164,7 +207,7 @@ test("correctly calculate base power for speed related moves", () => {
 		testFastPokemon,
 		testGyroBall,
 	);
-	expect(basePowerGyroBall).toBe(100);
+	expect(basePowerGyroBall.operator).toBe(100);
 });
 
 test("correctly calculate base power for Grass knot and Low kick", () => {
@@ -184,38 +227,38 @@ test("correctly calculate base power for Grass knot and Low kick", () => {
 		testHaavyPokemon,
 		grassKnot,
 	);
-	expect(basePowerLowKick).toBe(120);
-	expect(basePowerGrassknot).toBe(120);
+	expect(basePowerLowKick.operator).toBe(120);
+	expect(basePowerGrassknot.operator).toBe(120);
 
 	testHaavyPokemon.weight = 199;
 	basePowerLowKick = getBasePower(testPokemon, testHaavyPokemon, lowKick);
 	basePowerGrassknot = getBasePower(testPokemon, testHaavyPokemon, grassKnot);
-	expect(basePowerLowKick).toBe(100);
-	expect(basePowerGrassknot).toBe(100);
+	expect(basePowerLowKick.operator).toBe(100);
+	expect(basePowerGrassknot.operator).toBe(100);
 
 	testHaavyPokemon.weight = 99;
 	basePowerLowKick = getBasePower(testPokemon, testHaavyPokemon, lowKick);
 	basePowerGrassknot = getBasePower(testPokemon, testHaavyPokemon, grassKnot);
-	expect(basePowerLowKick).toBe(80);
-	expect(basePowerGrassknot).toBe(80);
+	expect(basePowerLowKick.operator).toBe(80);
+	expect(basePowerGrassknot.operator).toBe(80);
 
 	testHaavyPokemon.weight = 49;
 	basePowerLowKick = getBasePower(testPokemon, testHaavyPokemon, lowKick);
 	basePowerGrassknot = getBasePower(testPokemon, testHaavyPokemon, grassKnot);
-	expect(basePowerLowKick).toBe(60);
-	expect(basePowerGrassknot).toBe(60);
+	expect(basePowerLowKick.operator).toBe(60);
+	expect(basePowerGrassknot.operator).toBe(60);
 
 	testHaavyPokemon.weight = 24;
 	basePowerLowKick = getBasePower(testPokemon, testHaavyPokemon, lowKick);
 	basePowerGrassknot = getBasePower(testPokemon, testHaavyPokemon, grassKnot);
-	expect(basePowerLowKick).toBe(40);
-	expect(basePowerGrassknot).toBe(40);
+	expect(basePowerLowKick.operator).toBe(40);
+	expect(basePowerGrassknot.operator).toBe(40);
 
 	testHaavyPokemon.weight = 9;
 	basePowerLowKick = getBasePower(testPokemon, testHaavyPokemon, lowKick);
 	basePowerGrassknot = getBasePower(testPokemon, testHaavyPokemon, grassKnot);
-	expect(basePowerLowKick).toBe(20);
-	expect(basePowerGrassknot).toBe(20);
+	expect(basePowerLowKick.operator).toBe(20);
+	expect(basePowerGrassknot.operator).toBe(20);
 });
 
 test("correctly calculate base power for heavy slam & heat crash", () => {
@@ -233,62 +276,98 @@ test("correctly calculate base power for heavy slam & heat crash", () => {
 	});
 	let basePowerHeavySlam = getBasePower(testAttacker, testDefender, heavySlam);
 	let basePowerHeatCrash = getBasePower(testAttacker, testDefender, heatcrash);
-	expect(basePowerHeavySlam).toBe(40);
-	expect(basePowerHeatCrash).toBe(40);
+	expect(basePowerHeavySlam.operator).toBe(40);
+	expect(basePowerHeatCrash.operator).toBe(40);
 
 	testAttacker.weight = 25;
 	basePowerHeavySlam = getBasePower(testAttacker, testDefender, heavySlam);
 	basePowerHeatCrash = getBasePower(testAttacker, testDefender, heatcrash);
-	expect(basePowerHeavySlam).toBe(60);
-	expect(basePowerHeatCrash).toBe(60);
+	expect(basePowerHeavySlam.operator).toBe(60);
+	expect(basePowerHeatCrash.operator).toBe(60);
 
 	testAttacker.weight = 35;
 	basePowerHeavySlam = getBasePower(testAttacker, testDefender, heavySlam);
 	basePowerHeatCrash = getBasePower(testAttacker, testDefender, heatcrash);
-	expect(basePowerHeavySlam).toBe(80);
-	expect(basePowerHeatCrash).toBe(80);
+	expect(basePowerHeavySlam.operator).toBe(80);
+	expect(basePowerHeatCrash.operator).toBe(80);
 
 	testAttacker.weight = 45;
 	basePowerHeavySlam = getBasePower(testAttacker, testDefender, heavySlam);
 	basePowerHeatCrash = getBasePower(testAttacker, testDefender, heatcrash);
-	expect(basePowerHeavySlam).toBe(100);
-	expect(basePowerHeatCrash).toBe(100);
+	expect(basePowerHeavySlam.operator).toBe(100);
+	expect(basePowerHeatCrash.operator).toBe(100);
 
 	testAttacker.weight = 50;
 	basePowerHeavySlam = getBasePower(testAttacker, testDefender, heavySlam);
 	basePowerHeatCrash = getBasePower(testAttacker, testDefender, heatcrash);
-	expect(basePowerHeavySlam).toBe(120);
-	expect(basePowerHeatCrash).toBe(120);
+	expect(basePowerHeavySlam.operator).toBe(120);
+	expect(basePowerHeatCrash.operator).toBe(120);
 });
 
 test("base power of weather ball", () => {
 	const testMon = genTestMon();
 	const weatherBall = createMove({ id: 311, base: 50 });
 	let basePower = getBasePower(testMon, testMon, weatherBall);
-	expect(basePower).toBe(50);
+	expect(basePower).toEqual({
+		operator: 50,
+	});
 	// sun
 	basePower = getBasePower(testMon, testMon, weatherBall, { weather: "Sun" });
-	expect(basePower).toBe(100);
+	expect(basePower).toEqual({
+		operator: 100,
+		factors: {
+			field: {
+				weather: true
+			}
+		}
+	});
 	// rain
 	basePower = getBasePower(testMon, testMon, weatherBall, { weather: "Rain" });
-	expect(basePower).toBe(100);
+	expect(basePower).toEqual({
+		operator: 100,
+		factors: {
+			field: {
+				weather: true
+			}
+		}
+	});
 	// sand
 	basePower = getBasePower(testMon, testMon, weatherBall, { weather: "Sand" });
-	expect(basePower).toBe(100);
+	expect(basePower).toEqual({
+		operator: 100,
+		factors: {
+			field: {
+				weather: true
+			}
+		}
+	});
 	// snow
 	basePower = getBasePower(testMon, testMon, weatherBall, { weather: "Snow" });
-	expect(basePower).toBe(100);
+	expect(basePower).toEqual({
+		operator: 100,
+		factors: {
+			field: {
+				weather: true
+			}
+		}
+	});
 });
 
 test("base power of tera blast", () => {
 	const testMon = genTestMon();
 	const teraBlast = createMove({ id: 851, base: 80 });
 	let basePower = getBasePower(testMon, testMon, teraBlast);
-	expect(basePower).toBe(80);
+	expect(basePower.operator).toBe(80);
 	const testTeraStellarMon = genTestMon();
 	testTeraStellarMon.toggleTera({ isTera: true, type: "Stellar" });
 	basePower = getBasePower(testTeraStellarMon, testMon, teraBlast);
-	expect(basePower).toBe(100);
+	expect(basePower).toEqual({
+		operator: 100, factors: {
+			attacker: {
+				isTera: true
+			}
+		}
+	});
 });
 
 test("base power of Power Trip & Stored Power", () => {
@@ -306,6 +385,6 @@ test("base power of Power Trip & Stored Power", () => {
 	const basePowerPowerTrip = getBasePower(testMon, testMon, powerTrip);
 	const basePowerStoredPower = getBasePower(testMon, testMon, storedPower);
 	const expected = 20 + (6 + 5 + 4) * 20;
-	expect(basePowerPowerTrip).toBe(expected);
-	expect(basePowerStoredPower).toBe(expected);
+	expect(basePowerPowerTrip.operator).toBe(expected);
+	expect(basePowerStoredPower.operator).toBe(expected);
 });
