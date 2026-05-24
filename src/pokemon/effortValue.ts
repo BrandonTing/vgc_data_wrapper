b4c5ece2945bb2da47a1d050575233dba7d1959b
Brandon Ting <brandon1221t@gmail.com>
2026-05-24 20:35:30 +0900

    Remove unused getMaxEvPerStat export

--- src/pokemon/effortValue.ts
+++ src/pokemon/effortValue.ts
@@ -1,6 +1,6 @@
 import type { StatRuleset } from "./base";
 
-export const getMaxEvPerStat = (ruleset: StatRuleset) =>
+const getMaxEvPerStat = (ruleset: StatRuleset) =>
 	ruleset === "mainSeries" ? 252 : 32;
 
 export const getMaxTotalEvs = (ruleset: StatRuleset) =>
